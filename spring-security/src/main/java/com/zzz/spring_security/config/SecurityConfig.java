e5851c3ee943960d53710aa7b5a176d8a927016c
zmuhsenz <zmohamedmohsnz@gmail.com>
2025-03-15 17:40:19 +0200

    load user details using InMemoryUserDetailsManager

--- spring-security/src/main/java/com/zzz/spring_security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/zzz/spring_security/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +27,28 @@ public class SecurityConfig {
         // disable csrf
         .csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("mohsen")
+                .password("{noop}zzz")
+                .roles("user")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("shehata")
+                .password("{noop}zzz")
+                .roles("user", "manager")
+                .build();
+
+        UserDetails user3 = User.builder()
+                .username("amr")
+                .password("{noop}zzz")
+                .roles("user", "manager", "admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
     }
 }
